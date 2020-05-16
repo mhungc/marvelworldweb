2c018eec5b97bfeb23a8846f0d736e8219a61f01
CORP\mhungcam <marco-luis.hung-campos@sogeti.com>
2020-05-16 12:29:31 +0200

    Change dockerfile and other stuff

--- marvelworld-web/src/App.tsx
+++ marvelworld-web/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MarvelList from './MarvelList/MarvelList';
-import { windowConfig } from './Common/WindowConfig';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MarvelList from "./MarvelList/MarvelList";
+import { windowConfig } from "./Common/WindowConfig";
+import env from "@beam-australia/react-env";
 
 function App() {
-
-console.log(windowConfig);
+  console.log(windowConfig);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        MODIFIED 3
+          Edit <code>src/App.tsx</code> and save to reload. MODIFIED 3
         </p>
-  <div>envirorment REACT_APP_URLMARVEL: {process.env.REACT_APP_URLMARVEL}</div>
-  <div>envirorment REACT_APP_INMINUTU_API_URL: {process.env.REACT_APP_API_URL}</div>
-    <p> REACT_APP_API_URL runtime variable: { windowConfig._env_ .REACT_APP_API_URL} </p>
-        <MarvelList/>
+        <div>
+        <small>
+            Current environment2: <b>{env("NODE_ENV")}</b>.
+          </small>
+          <small>
+           | REACT_APP_API_URL2: <b>{env("API_URL")}</b>.
+          </small>
+          <small>
+           | REACT_APP_MARVE2L: <b>{env("MARVEL")}</b>.
+          </small>
+          
+        
+        </div>
+        <MarvelList />
         <a
           className="App-link"
           href="https://reactjs.org"
--- marvelworld-web/src/MarvelList/MarvelList.tsx
+++ marvelworld-web/src/MarvelList/MarvelList.tsx
@@ -1,53 +1,41 @@
 import React, { useState, useEffect } from "react";
 import Characters from "./MarvelListInterfaces";
-import axios from 'axios';
-import { windowConfig } from "../Common/WindowConfig";
+import axios from "axios";
+import env from "@beam-australia/react-env";
 
 function MarvelList() {
+  const defaultCharactersList: Array<Characters> = [];
 
-const defaultCharactersList: Array<Characters> = [];
+  const [charactersList, setCharacters] = useState<Array<Characters>>(
+    defaultCharactersList
+  );
+  console.log(env("MARVEL")+"/api/MarverlWorld");
+  //.get('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=714bb5a21a336a01dc4205c7f1146991&hash=823030e123364bed2249367876fb0b6c')
+  useEffect(() => {
+    axios.get(env("MARVEL")+"/api/MarverlWorld").then((res) => {
+      let listMarvelCharacters: Array<Characters> = [];
+      res.data.forEach((value: any) => {
+        let character: Characters;
+        character = {
+          name: value.name,
+          thumbnail: `${value.thumbnail.path}.${value.thumbnail.extension}`,
+        };
 
-const [charactersList , setCharacters] = useState<Array<Characters>>(defaultCharactersList);
-//.get('https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=714bb5a21a336a01dc4205c7f1146991&hash=823030e123364bed2249367876fb0b6c')
-useEffect(() => {
-    axios.
-    get(windowConfig._env_ .REACT_APP_API_URL + '/api/MarverlWorld')
-    .then(res => {
-            let listMarvelCharacters: Array<Characters> = [];
-            res.data.forEach((value:any) => {
+        listMarvelCharacters.push(character);
+      });
 
-                let character: Characters;
-                character = {
-                        name: value.name,
-                        thumbnail: `${value.thumbnail.path}.${value.thumbnail.extension}`
-                };
-
-                listMarvelCharacters.push(character);
-            });
-
-            setCharacters(listMarvelCharacters);
+      setCharacters(listMarvelCharacters);
     });
-},[]);
+  }, []);
 
-    return(
-     <div>
-         {
-
-         
-         charactersList.map((character,index) => 
-             (
-                <li key={index}>
-                { character.name }
-              
-              </li>
-            //    <img src={character.thumbnail} alt="Italian Trulli"></img>
-             )
-         )}
-     </div>
- 
-    
-    );
-
+  return (
+    <div>
+      {charactersList.map((character, index) => (
+        <li key={index}>{character.name}</li>
+        //    <img src={character.thumbnail} alt="Italian Trulli"></img>
+      ))}
+    </div>
+  );
 }
 
 export default MarvelList;
